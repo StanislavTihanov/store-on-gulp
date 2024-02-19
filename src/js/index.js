ddea88f9177c9332784cd36948b0641b6b7c5b02
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2024-02-19 13:49:53 +0300

    Add Swiper slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    spaceBetween: 24,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  });
+
+
 //import mobileNav from './modules/mobile-nav.js';
 //mobileNav();
 
